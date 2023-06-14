621921c477409b9aada6ec9eab37b44222a03531
aprokopetsphilips <aprokopetsphilips@gmail.com>
2023-06-14 15:58:37 +0300

    форма вторизации и редиректы

--- flsite.py
+++ flsite.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for,request,flash
+from flask import Flask, render_template,url_for,request,flash,session,redirect,abort
 
 app = Flask(__name__)
 
@@ -22,11 +22,35 @@ def about():
 def contact():
     if request.method == 'POST':
         if len(request.form['username']) > 2:
-            flash('Сообщение отправлено')
+            flash('Сообщение отправлено', category='success')
         else:
-            flash('Не смог')
+            flash('Не смог', category='error')
 
     return render_template('contact.html', title = 'Обратная связь', menu=menu)
+
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'andrey' and request.form['psw'] == 'andrey':
+        session['userLogged'] = request.form['username']
+        return  redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title='Авторизация', menu=menu)
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f'Профиль пользователя:{username}'
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page404.html', title='Страница не найдена', menu=menu)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
