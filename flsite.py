9977561b6ff4a090c59b88b24de2bbe286be009a
aprokopetsphilips <aprokopetsphilips@gmail.com>
2023-06-15 12:03:35 +0300

    прикручиваю бд

--- flsite.py
+++ flsite.py
@@ -1,21 +1,78 @@
-from flask import Flask, render_template,url_for,request,flash,session,redirect,abort
-
+from flask import Flask, render_template,url_for,request,flash,session,redirect,abort, g
+from FDataBase import FDataBase
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '1234567890qwerty'
 
-menu = [{'name':'Установка', 'url': 'install-flask'},
+"""menu = [{'name':'Установка', 'url': 'install-flask'},
         {'name':'Первое приложение', 'url': 'first-app'},
         {'name':'Обратная связь', 'url': 'contact'}
         ]
+"""
 
 
+import os
+import sqlite3
+
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+#SECRET_KEY = '123456789qwerty'
+app.config.from_object(__name__) # загрузка конфигурации непосредствено из приложения(__name__ ссылается на текущий файл)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return  g.link_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
 @app.route('/')
 def index():
+    db = get_db()
+    dbase = FDataBase(db)
+    menu = dbase.getMenu()
     return render_template('index.html', menu=menu)
 
+@app.route('/add_post', methods=['POST', 'GET'])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
 
-@app.route('/about')
+    if request.method == 'POST':
+        if len(request.form['name']) >4 and len(request.form['post'])>10:
+            res = dbase.addPost(request.form['name'],request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+    return render_template('add_post.html',menu = dbase.getMenu(), title='Добавление статьи' )
+
+
+
+
+"""@app.route('/')
+def index():
+    return render_template('index.html', menu=menu)"""
+
+"""@app.route('/about')
 def about():
     return render_template('about.html', menu=menu, title='О сайте')
 @app.route('/contact', methods=['POST','GET'])
@@ -43,14 +100,14 @@ def login():
 def profile(username):
     if 'userLogged' not in session or session['userLogged'] != username:
         abort(401)
-    return f'Профиль пользователя:{username}'
+    return f'Профиль пользователя:{username}'"""
 
 
-@app.errorhandler(404)
+"""@app.errorhandler(404)
 def pageNotFound(error):
     return render_template('page404.html', title='Страница не найдена', menu=menu)
 
-
+"""
 
 if __name__ == '__main__':
     app.run(debug=True)
